791e8d00b211a229ef1f6fe4c6c9b9d90b2df53a
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-21 20:04:22 +0200

    create register views

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+from .models import User
+from .forms import UserRegisterForm
+
 
 # Create your views here.
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"You are already logged in.")
+        return redirect("account:account")
+
+    
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data['username']
+
+            messages.success(request, f'Account created for {username}! You are now able to log in.')
+
+            new_user = authenticate(
+                username = form.cleaned_data['email'],
+                password = form.cleaned_data['password1'],
+            )
+
+            login(request, new_user)
+            return redirect("account:account")
+        
+
+    else:
+        
+        form = UserRegisterForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request,'userauths/sign-up.html', context)
