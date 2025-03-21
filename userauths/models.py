c21b14c2a0c9124cb21f0646b1c57bc2af80e43e
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-21 22:15:08 +0200

    desing profile models

--- userauths/models.py
+++ userauths/models.py
@@ -68,3 +68,43 @@ def save_account(sender, instance,**kwargs):
 
 post_save.connect(create_account, sender=User)
 post_save.connect(save_account, sender=User)
+
+
+
+MARITAL_STATUS = (
+    ("married", "Married"),
+    ("single", "Single"),
+    ("other", "Other")
+)
+
+GENDER = (
+    ("male", "Male"),
+    ("female", "Female"),
+    ("other", "Other")
+)
+
+
+
+class KYC(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user =  models.OneToOneField(User, on_delete=models.CASCADE)
+    account =  models.OneToOneField(Account, on_delete=models.CASCADE, null=True, blank=True)
+    frist_name = models.CharField(max_length=1000)
+    last_name = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to="kyc", default="default.jpg")
+    marrital_status = models.CharField(choices=MARITAL_STATUS, max_length=40)
+    gender = models.CharField(choices=GENDER, max_length=40)
+    date_of_birth = models.DateTimeField(auto_now_add=False)
+    signature = models.ImageField(upload_to="kyc")
+    Company = models.CharField(max_length=100)
+    # Contact Detail
+    mobile = models.CharField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user}"    
+
+    
+    class Meta:
+        ordering = ['-date']
