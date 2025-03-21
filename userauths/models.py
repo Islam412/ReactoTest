a309aaee93328e5280194294a9fa7f8e30729a53
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-21 21:48:51 +0200

    desing defult py user

--- userauths/models.py
+++ userauths/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 import uuid
 from shortuuid.django_fields import ShortUUIDField
@@ -56,3 +57,14 @@ class Account(models.Model):
 
     def __str__(self):
         return f"{self.user}"
+    
+
+def create_account(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance)
+
+def save_account(sender, instance,**kwargs):
+    instance.account.save()
+
+post_save.connect(create_account, sender=User)
+post_save.connect(save_account, sender=User)
