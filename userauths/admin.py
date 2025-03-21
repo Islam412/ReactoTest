2d58cb591f0c3c417f99f9c180fd4b6c7963c4fc
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-21 22:25:12 +0200

    call account and kyc at admin banal

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from userauths.models import User
+from userauths.models import User , Account , KYC
 
 
 # Register your models here.
@@ -18,5 +18,18 @@ class UserCustomAdmin(admin.ModelAdmin):
     filter_horizontal = ('user_permissions',)
 
 
+class AccountAdminModel(ImportExportModelAdmin):
+    list_editable = ['account_status', 'account_balance', 'kyc_submitted', 'kyc_confirmed'] 
+    list_display = ['user', 'account_number' ,'account_status', 'kyc_submitted', 'kyc_confirmed'] 
+    list_filter = ['account_status']
+    search_fields = ['user__username', 'user__email', 'account_number']
+
+
+class KYCAdmin(ImportExportModelAdmin):
+    search_fields = ["frist_name" , "last_name" , "account"]
+    list_display = ['user', 'frist_name' , "last_name", 'gender', 'date_of_birth'] 
+
 
 admin.site.register(User, UserCustomAdmin)
+admin.site.register(Account, AccountAdminModel)
+admin.site.register(KYC, KYCAdmin)
--- userauths/models.py
+++ userauths/models.py
@@ -39,7 +39,6 @@ ACCOUNT_STATUS = (
 class Account(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     user =  models.OneToOneField(User, on_delete=models.CASCADE)
-    account_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     account_number = ShortUUIDField(unique=True,length=10, max_length=25, prefix="217", alphabet="1234567890")
     account_id = ShortUUIDField(unique=True,length=7, max_length=25, prefix="DEX", alphabet="1234567890")
     pin_number = ShortUUIDField(unique=True,length=4, max_length=7, alphabet="1234567890") #2737
