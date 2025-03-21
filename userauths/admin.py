3afded83b4ed511df95328a9fb56e3fafd90c350
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-21 19:56:37 +0200

    add static and media file

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from userauths.models import User
+
 
 # Register your models here.
+
+
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    search_fields = ['username', 'email']
+    list_filter = ['email']
+    ordering = ['email']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    filter_horizontal = ('user_permissions',)
+
+
+
+admin.site.register(User, UserCustomAdmin)
