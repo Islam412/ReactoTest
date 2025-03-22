dce50eff438809fc2657353bea4afd00c492c339
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-22 03:18:07 +0200

    create kyc forms

--- userauths/forms.py
+++ userauths/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ImageField, FileInput, DateInput
 
-from .models import User
+from .models import User , KYC
 
 
 class UserRegisterForm(UserCreationForm):
@@ -13,3 +14,27 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class KYCForm(forms.ModelForm):
+    identity_image = ImageField(widget=FileInput)
+    image = ImageField(widget=FileInput)
+    signature = ImageField(widget=FileInput)
+
+    class Meta:
+        model = KYC
+        fields = [ 'full_name', 'image', 'marrital_status', 'gender', 'identity_type', 'identity_image', 'date_of_birth', 'signature', 'country', 'state', 'city', 'mobile', 'fax']
+        widgets = {
+            "full_name": forms.TextInput(attrs={"placeholder":"Full Name"}),
+            "mobile": forms.TextInput(attrs={"placeholder":"Mobile Number"}),
+            "fax": forms.TextInput(attrs={"placeholder":"Fax Number"}),
+            "country": forms.TextInput(attrs={"placeholder":"Country"}),
+            "state": forms.TextInput(attrs={"placeholder":"State"}),
+            "city": forms.TextInput(attrs={"placeholder":"City"}),
+            'date_of_birth':DateInput
+        }
