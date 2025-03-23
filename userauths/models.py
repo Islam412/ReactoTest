77af503865d726063a8b5aebb5a65c4622279643
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-24 01:39:08 +0200

    update user mobels p3

--- userauths/models.py
+++ userauths/models.py
@@ -19,13 +19,13 @@ def user_directory_path(instance, filename):
 
 
 class User(AbstractUser):
-    frist_name = models.CharField(_('Username'), max_length=255)
-    last_name = models.CharField(_('Username'), max_length=255)
+    frist_name = models.CharField(_('Frist Name'), max_length=255)
+    last_name = models.CharField(_('Last Name'), max_length=255)
     username = models.CharField(_('Username'), max_length=255)
     email = models.EmailField(_('Email'), unique=True)
-    phone = PhoneNumberField(_("Phone Number"), unique=True)
-    date_of_birth = models.DateTimeField(_('Date Of Birth'), auto_now_add=False)
-    company = models.CharField(_('Company'), max_length=255)
+    phone = PhoneNumberField(_("Phone Number"), unique=True, null=True, blank=True)
+    date_of_birth = models.DateTimeField(_('Date Of Birth'), auto_now_add=False , null=True, blank=True)
+    company = models.CharField(_('Company'), max_length=255 , null=True, blank=True)
     is_staff = models.BooleanField(_('Is Staff'), default=False)
     is_superuser = models.BooleanField(_('Super User'), default=False)
 
