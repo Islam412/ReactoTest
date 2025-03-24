4344cee23ea2f46b0d9973d7a4348bfc20a4885e
Islam Hamdy <islam.124.hamdy@gmail.com>
2025-03-24 02:15:40 +0200

    update user register form

--- userauths/forms.py
+++ userauths/forms.py
@@ -1,20 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm , PasswordChangeForm
 from django.forms import ImageField, FileInput, DateInput
+from phonenumber_field.formfields import PhoneNumberField
 
 
 from .models import User , KYC
 
 
 class UserRegisterForm(UserCreationForm):
+    frist_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Frist Name"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Last Name"}))
     username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"Email"}))
+    phone = PhoneNumberField(widget=forms.NumberInput(attrs={"placeholder": "Phone"}))
+    date_of_birth = forms.DateTimeField(widget=forms.DateInput(attrs={"type": "date", "placeholder": "Date of Birth"}))
+    company = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Company"}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['frist_name', 'last_name', 'username', 'email', 'phone', 'date_of_birth', 'company', 'password1', 'password2']
 
 
 
